6a8eda1dfb66c78ad53fa62ac3a9f7d20f65c27c
Sifatullah Jan <Sifatullah0314@yahoo.com>
2019-08-21 21:21:25 +0500

    addedfile

---
+++ lab8_3a_web.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> Lab 8 Task 2 Web </title>
+<style type = "text/css">
+h1{font-family:Garamond;font-weight:bold; text-align:center;}
+</style>
+</head>
+<body>
+<h1> TASK 2 </h1>
+	<div style="border-style:groove;">
+		<div align = "center" style="margin-top:5%;margin-bottom:5%;">
+			<form action="lab8_3a_insert.php" method="post">
+				EmpID       : <input type = "text" name = "EmpID"> <br><br>
+				empName : <input type = "text" name = "empName"> <br><br>
+				depID        : <input type = "text" name = "depID"> <br><br>
+				job             : <input type = "text" name = "job"> <br><br>
+				city            : <input type = "text" name = "city"> <br><br>
+				<button> INSERT </button>
+			</form>
+		</div>
+	</div>
+</body>
+</html>
